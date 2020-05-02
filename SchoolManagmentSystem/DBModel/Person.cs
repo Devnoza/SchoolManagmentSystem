2cf02e22a0d1340b2718be6963c114344b76ffe6
SABA <SABA@DESKTOP-H8AKDBG>
2020-05-02 23:06:48 +0400

    first step

---
+++ SchoolManagmentSystem/DBModel/Person.cs
@@ -0,0 +1,43 @@
+namespace DBModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Person")]
+    public partial class Person
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Person()
+        {
+            Students = new HashSet<Student>();
+            Teachers = new HashSet<Teacher>();
+            Users = new HashSet<User>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string LastName { get; set; }
+
+        public bool Gender { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Student> Students { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Teacher> Teachers { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
---
+++ SchoolManagmentSystem/DBModel/Role.cs
@@ -0,0 +1,31 @@
+namespace DBModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Role")]
+    public partial class Role
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Role()
+        {
+            Users = new HashSet<User>();
+            Permissions = new HashSet<Permission>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<User> Users { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Permission> Permissions { get; set; }
+    }
+}
