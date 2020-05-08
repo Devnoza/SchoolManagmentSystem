d5788fd99785329eb3cc346d4ff5183eb2d8f303
Guga jorjikia <jorjikiaguga@gmail.com>
2020-05-08 17:28:27 +0400

    Added Login

---
+++ SchoolManagmentSystem/SchoolManagmentSystem/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DBModel;
+
+namespace SchoolManagmentSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult LoginView()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginView(User incominguser)
+        {
+            using (Context context = new Context())
+            {
+                User user = context.Users
+                .Where(u => u.UserName == incominguser.UserName && u.Password == incominguser.Password)
+                .FirstOrDefault();
+
+                if (user != null)
+                {
+                    Session["Username"] = user.UserName;
+                    Session["UserId"] = user.Id;
+                    
+                    return RedirectToAction("index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid Username or Password");
+                    return View(incominguser);
+                }
+            }
+        }
+    }
+}
