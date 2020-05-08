21caa963e2fc21dd735562fb8c9c329f5e1caade
nicksva <64211570+nicksva@users.noreply.github.com>
2020-05-09 03:11:41 +0400

    j

---
+++ SchoolManagmentSystem/SchoolManagmentSystem/authorization/CustomAuthenticationFilter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagmentSystem.authorization
+{
+    public class Class1
+    {
+    }
+}
