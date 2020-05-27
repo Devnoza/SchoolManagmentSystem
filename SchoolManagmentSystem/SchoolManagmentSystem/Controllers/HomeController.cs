14de30582d315854d53497daec3424ea45e8c4c3
Nika374 <nikadavitaia374@gmail.com>
2020-05-27 23:34:40 +0400

    Added Authorization

--- SchoolManagmentSystem/SchoolManagmentSystem/Controllers/HomeController.cs
+++ SchoolManagmentSystem/SchoolManagmentSystem/Controllers/HomeController.cs
@@ -4,24 +4,45 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DBModel;
+using SchoolManagmentSystem.Filters;
 
 namespace SchoolManagmentSystem.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {   
+            return View();
+        }
+        [CustomAuthorize("Administrator")]
+        public ActionResult UserView()
         {
             Context Co = new Context();
+
             return View(Co.Users.ToList());
         }
+        public ActionResult TeacherView()
+        {
+            Context Co = new Context();
 
+            return View(Co.Teachers.ToList());
+        }
+        public ActionResult StudenView()
+        {
+            Context Co = new Context();
+
+            return View(Co.Students.ToList());
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+        public ActionResult NoRole()
+        {
+            return View();
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
---
+++ SchoolManagmentSystem/SchoolManagmentSystem/Filters/CustomAuthorizeAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using DBModel;
+
+namespace SchoolManagmentSystem.Filters
+{
+        public class CustomAuthorizeAttribute : AuthorizeAttribute
+        {
+            private readonly string[] allowedroles;
+            public CustomAuthorizeAttribute(params string[] roles)
+            {
+                this.allowedroles = roles;
+            }
+            protected override bool AuthorizeCore(HttpContextBase httpContext)
+            {
+                bool authorize = false;
+                var userId = Convert.ToInt32(httpContext.Session["UserId"]);
+                if (userId != 0)
+                    using (var Context = new Context())
+                    {
+                        var userRole = Context.Users.Where(o => o.Id == userId).FirstOrDefault().Role.Name;
+                        foreach (var role in allowedroles)
+                        {
+                            if (role == userRole) return true;
+                        }
+                    }
+
+
+                return authorize;
+            }
+
+            protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                   new RouteValueDictionary
+                   {
+                    { "controller", "Home" },
+                    { "action", "NoRole" }
+                   });
+            }
+        }
+    }
